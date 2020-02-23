b87a155fcc24e2a296a5fc243ab8855b065204fc
youchao liu <ryuforever@163.com>
2020-02-23 16:57:29 +0800

    update `package.json` to support lint, prettier, webpack-dev

---
+++ examples/basic/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Basic = () => (
+  <div>
+    hello world
+  </div>
+)
+
+ReactDOM.render(<Basic />, document.getElementById('app'))
