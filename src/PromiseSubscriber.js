eac17c70fb2d6a43e9956a710dbb4048656e9dbf
youchao liu <ryuforever@163.com>
2020-02-25 00:39:16 +0800

    fix update promise directly

--- src/PromiseSubscriber.js
+++ src/PromiseSubscriber.js
@@ -5,35 +5,44 @@ export default class PromiseSubscriber {
   constructor({config, fetcher}) {
     this.rejecter = () => {}
     this.resolver = () => {}
-    this.promise = Promise.resolve()
+    this.promise = resumablePromise()
+    console.log("this promise ", this.promise)
 
     this.fetcher = fetcher
+    this.fetcher.promise.then(
+      result => {
+        this.promise.hooks.onFulfilled(result)
+      },
+      error => {
+        this.promise.hooks.onReject(error)
+      }
+    )
 
     // hook then, it will return a new promise...
-    Object.defineProperty(this.promise, "then", {
-      ...thenDescriptor,
-      value: function(_onFulfilled, _onRejected) {
-        this.onFulfilled = _onFulfilled
-        this.onReject = _onRejected
-        this.fetcher.promise.then(this.onFulfilled, this.onReject)
-      }.bind(this)
-    })
+    // Object.defineProperty(this.promise, "then", {
+    //   ...thenDescriptor,
+    //   value: function(_onFulfilled, _onRejected) {
+    //     this.onFulfilled = _onFulfilled
+    //     this.onReject = _onRejected
+    //     this.fetcher.promise.then(this.onFulfilled, this.onReject)
+    //   }.bind(this)
+    // })
 
-    Object.defineProperty(this.promise, "catch", {
-      ...catchDescriptor,
-      value: function(_onCatch) {
-        this.onCatch = _onCatch
-        this.fetcher.promise.catch(this.onCatch)
-      }.bind(this)
-    })
+    // Object.defineProperty(this.promise, "catch", {
+    //   ...catchDescriptor,
+    //   value: function(_onCatch) {
+    //     this.onCatch = _onCatch
+    //     this.fetcher.promise.catch(this.onCatch)
+    //   }.bind(this)
+    // })
 
-    Object.defineProperty(this.promise, "finally", {
-      ...finallyDescriptor,
-      value: function(_onFinally) {
-        this.onFinally = _onFinally
-        this.fetcher.promise.finally(this.onFinally)
-      }.bind(this)
-    })
+    // Object.defineProperty(this.promise, "finally", {
+    //   ...finallyDescriptor,
+    //   value: function(_onFinally) {
+    //     this.onFinally = _onFinally
+    //     this.fetcher.promise.finally(this.onFinally)
+    //   }.bind(this)
+    // })
 
     this.retryTimeoutHandler = null
     this.poolTimeoutHandler = null
