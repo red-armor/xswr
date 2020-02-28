e69a3db34cd795e9b59a0eb2c0b3740d8b78f402
youchao liu <ryuforever@163.com>
2020-02-28 22:06:20 +0800

    update `retryStrategy` and `poolStrategy`

--- src/PoolStrategy.js
+++ src/PoolStrategy.js
@@ -1,1 +1,32 @@
-export default class PoolStrategy {}
+export default class PoolStrategy {
+  constructor({poolInterval}) {
+    this.belongs = null
+    this.timeoutHandler = null
+    this.interval = poolInterval
+  }
+
+  bind(subscriber) {
+    this.belongs = subscriber
+  }
+
+  suspense() {
+    if (this.timeoutHandler) {
+      clearTimeout(this.timeoutHandler)
+      this.timeoutHandler = null
+    }
+  }
+
+  resumeTick() {
+    if (!shouldIStartPool) return
+    this.timeoutHandler = setTimeout(() => {
+      this.belongs.validate()
+    })
+  }
+
+  cleanup() {
+    if (this.timeoutHandler) {
+      clearTimeout(this.timeoutHandler)
+      this.timeoutHandler = null
+    }
+  }
+}
