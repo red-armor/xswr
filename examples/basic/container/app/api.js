739c2473f102722218e854c6d780ee8b2426f05e
youchao liu <ryuforever@163.com>
2020-02-29 17:59:44 +0800

    fix `attemptToFetch`

--- examples/basic/container/app/api.js
+++ examples/basic/container/app/api.js
@@ -1,14 +1,32 @@
-export const getInfo = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        data: [
-          {
-            location: "shanghai"
-          }
-        ],
-        success: true
-      })
-    }, 1000)
-  })
+export const getInfo = (url, params) => {
+  if (url === "/api/info") {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: [
+            {
+              location: "shanghai"
+            }
+          ],
+          success: true
+        })
+      }, 1000)
+    })
+  }
+
+  if (url === "/api/city") {
+    const {city} = params
+    const list = {
+      shanghai: {a: 1}
+    }
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: list[city],
+          success: true
+        })
+      }, 1000)
+    })
+  }
 }
